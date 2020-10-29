626132172c64bf808157e16a0a4bee3640a2bb19
thehardwareguy2000 <54811767+thehardwareguy2000@users.noreply.github.com>
2020-10-29 16:12:11 +0530

    final

--- components/Education.js
+++ components/Education.js
@@ -11,7 +11,7 @@ export default class Education extends React.Component {
                 <Item 
                         time="January 2020 – April 2020"
                         name="Speech to Emotion Detection System"
-                    
+                        url="https://github.com/thehardwareguy2000/Speech-to-emotion-detection"
                         color="#28FFEB"
                     />
 
@@ -19,17 +19,26 @@ export default class Education extends React.Component {
                     <Item 
                         time="December 2019 – January 2020"
                         name="Employee Management App (Java)"
-                        
+                        url="https://github.com/thehardwareguy2000/Java-Class/tree/master/Project"
                         color="#2ecc71"
+                    />
+                    <Item 
+                        time="August 2019 – september 2019"
+                        name="Machine Learning Algorithm Traning"
+                        url="https://thehardwareguy2000.github.io/Dwm-project/"
+                        color="#FF5733"
                     />
                     <Item 
                         time="July 2019 – October 2019"
                         name="Smart Attendance"
+                        url="https://github.com/thehardwareguy2000/Smart-Attendance"
                         color="#3498db"
                     />
+                     
                     <Item 
                         time="July 2019 – October 2019"
                         name="Malaria Detection App"
+                        url="https://github.com/thehardwareguy2000/Malaria_Detection_app"
                         color="#FF5733"
                     />
                    
